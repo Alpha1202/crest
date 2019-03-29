f9233c652f9be1f49a4d40d751fca45dfff136dc
Nzubechukwu <nzubennamani@gmail.com>
2019-03-29 04:05:42 +0100

    set up the necessary tools for api dev

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
-module.exports = {
-     "extends": "airbnb-base"
+export default {
+     "extends": "airbnb-base",
+     "rules": {
+          "linebreak-style": 0,
+          "global-require": 0,
+          "eslint linebreak-style": [0, "error", "windows"]
+     }
 };
