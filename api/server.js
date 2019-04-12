40ee435b4da7ec99c5057642dd669797471225ef
Nzubechukwu <nzubennamani@gmail.com>
2019-04-12 10:24:58 +0100

    chore(deploy to heroku):deploy to heroku  [#165290805]

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyparser from 'body-parser';
 import dotenv from 'dotenv';
-import router from './api/routes';
+import router from './routes';
 
 dotenv.config();
 
