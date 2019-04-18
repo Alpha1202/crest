070cdf28dc050e24b214f24905ca9a9ec233b840
Nzubechukwu <nzubennamani@gmail.com>
2019-04-18 22:58:55 +0100

    refactor ui pages

---
+++ checkUser.js
@@ -0,0 +1,16 @@
+const admin = document.getElementById('check');
+
+const signin = document.getElementById('signin');
+ 
+
+ signin.addEventListener('click', () => {
+  if(admin.value.toLowerCase() === "admin" || admin.value.toLowerCase() === "staff") {
+  location.replace('./admin-dashboard.html')
+ } 
+});
+
+signin.addEventListener('click', () => {
+if(admin.value.toLowerCase() === "client" || admin.value.toLowerCase() === "user") {
+  location.replace('./user-dashboard.html')
+ }
+});
