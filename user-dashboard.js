5f23d572093dc443b780b35dd830a01920e02c48
Nzubechukwu <nzubennamani@gmail.com>
2019-04-27 17:06:36 +0100

    refactored ui pages

--- user-dashboard.js
+++ user-dashboard.js
@@ -1,28 +1,30 @@
 
 let openDashboard = document.getElementById('dashboard');
 
-let sideMenu = document.getElementById('side');
+let sideMenu = document.querySelector('.top-box');
 
 let open = document.getElementById('links');
 
 let modal = document.getElementById('Modal');
 
 let close = document.querySelector('.modal-close');
-
-open.onclick = function(){
-    modal.style.display = 'flex';
+if (open !== null) {
+    open.onclick = function(){
+        modal.style.display = 'flex';
+    }
 }
+
 
 close.onclick = function(){
     modal.style.display = 'none';
 }
 
-openDashboard.onclick = function(){
-    sideMenu.style.display = 'flex';
-}
+openDashboard.addEventListener('click', () => {
+    sideMenu.classList.toggle('side');
+})
 
-// window.onclick = function(event) {
-//     if (event.target == sideMenu) {
-//         sideMenu.style.display = 'none';
-//     }
-// }
+window.onclick = function(event) {
+    if (event.target == sideMenu) {
+        sideMenu.style.display = 'none';
+    }
+}
